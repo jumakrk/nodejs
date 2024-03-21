9f5250e0ef27314b89cf5cb284657b2666e8876a
jumakrk <jumakrk@gmail.com>
2024-03-21 11:35:17 +0300

    added verifyRefreshToken in the jwtHelper.js file

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -57,23 +57,23 @@ module.exports = {
         resolve (token);
       })
     })
+  },
 
-    verifyRefreshToken: (req, res, next)=> {
-      if(!req.headers['authorization']) return next (createError.Unauthorized())
-      const authHeader = req.headers['authorization']
-      const bearerToken = authHeader.split(' ')
-      const token = bearerToken[1]
-      JWT.verify(token, process.env.Refresh_Token_SECRET, (err, payload)=>{
-        if(err){
-          if(err.name === 'JsonWebTokenError'){
-            return next(createError.Unauthorized())
-          }else{
-            return next(createError.Unauthorized(err.message))
-          }
+  verifyRefreshToken: (req, res, next)=> {
+    if(!req.headers['authorization']) return next (createError.Unauthorized())
+    const authHeader = req.headers['authorization']
+    const bearerToken = authHeader.split(' ')
+    const token = bearerToken[1]
+    JWT.verify(token, process.env.Refresh_Token_SECRET, (err, payload)=>{
+      if(err){
+        if(err.name === 'JsonWebTokenError'){
+          return next(createError.Unauthorized())
+        }else{
+          return next(createError.Unauthorized(err.message))
         }
-        req.payload = payload
-        next()
-      })
-    },
-  }
+      }
+      req.payload = payload
+      next()
+    })
+  },
 }
